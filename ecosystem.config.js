28b460b2d5b5c482ef49da6e92bab0095adcbe8c
Eric Freiberg <eric.freiberg@pointsource.com>
2017-11-19 15:55:30 -0600

    Update install paths

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,9 @@ module.exports = {
     {
       name: "weather_server",
       script: "server.js",
-      cwd: "/opt/weather-station",
-      error_file: "/opt/weather-station/logs/error.log",
-      out_file: "/opt/weather-station/logs/info.log",
+      cwd: "/opt/weather-station/weather-station",
+      error_file: "/opt/weather-station/weather-station/logs/error.log",
+      out_file: "/opt/weather-station/weather-station/logs/info.log",
       merge_logs: true,
       env: {
         NODE_ENV: "development"
